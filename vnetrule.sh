f899e7e1dbe3db7a13994eabc78797f85d15d134
Suresh kumar <serush46@users.noreply.github.com>
2018-07-06 14:36:55 +0530

    Update vnetrule.sh

--- vnetrule.sh
+++ vnetrule.sh
@@ -1,22 +1,19 @@
-label=$(date +%Y%m%d)
-
-
-passwd=$(az ad sp create-for-rbac --name suresh --query password -o tsv)
-echo $passwd
-sleep 5
-appid=$(az ad sp show --id http://suresh --query appId -o tsv)
-echo $appid
-tenantid=$(az ad sp show --id http://suresh --query appOwnerTenantId -o tsv)
-echo $tenantid
-
-#Get Subscription ID
-
-sub=$(az account list --output table | awk 'FNR == 3 {print $4}')
-
-#Generate access token
-token=$(curl -X POST -d 'grant_type=client_credentials&client_id="'$appid'"&client_secret="'$passwd'"&resource=https%3A%2F%2Fmanagement.azure.com%2F' https://login.microsoftonline.com/$tenantid/oauth2/token | jq .access_token | sed 's/\"//g')
-
+az keyvault key create --vault-name genpactvault$labels --name diskencryptkey -p software
+# Create an Azure Active Directory service principal for authenticating requests to Key Vault.
+# Read in the service principal ID and password for use in later commands.
+sp_password=$(az ad sp create-for-rbac -n diskencrypt --query password -o tsv)
+sp_id=$(az ad sp show --id http://diskencrypt --query appId -o tsv)
+# Grant permissions on the Key Vault to the AAD service principal.
+az keyvault set-policy --name genpactvault$labels --spn $sp_id \
+--key-permissions backup create decrypt encrypt get import list recover restore sign unwrapKey update verify wrapKey \
+--secret-permissions backup get list recover restore set
+# Encrypt the VM disks.
+az vm encryption enable --resource-group PhoenixCatalogStg --name gecastgml$label --aad-client-id $sp_id --aad-client-secret $sp_password --disk-encryption-keyvault genpactvault$labels --key-encryption-key diskencryptkey --volume-type all
 sleep 2
-#Create VNET Rule
-
-curl -X PUT -H "Authorization: Bearer $token" -H "Content-Type: application/json" --data '{ "properties": { "ignoreMissingVnetServiceEndpoint": false, "virtualNetworkSubnetId": "/subscriptions/'$sub'/resourceGroups/geeks_rg/providers/Microsoft.Network/virtualNetworks/Dtlminjar/subnets/DtlminjarSubnet" }}' https://management.azure.com/subscriptions/$sub/resourceGroups/geeks_rg/providers/Microsoft.Sql/servers/tdegenpacttest/virtualNetworkRules/sqlaccess$label?api-version=2015-05-01-preview
+enc=$(az vm encryption show -n gecastgml$label -g PhoenixCatalogStg --query osDiskEncryptionSettings.enabled -o tsv)
+encexpected=true 
+                if [ "$enc" = "$encexpected" ]; then
+                        msg "Encryption initiated, Please check the VM for progress"
+                else 
+                        error_exit "Encryption failed"
+                        exit 1
