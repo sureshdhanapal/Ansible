145725b3fff1b86b177a4b4fd91a0a76f634c4a7
Suresh kumar <serush46@users.noreply.github.com>
2018-12-04 15:10:27 +0530

    Delete Final_Predict.py

--- Final_Predict.py
+++
@@ -1,4 +0,0 @@
-https://github.com/jupyterlab/jupyterlab-git
-https://github.com/jupyterlab/jupyterlab-github
-https://github.com/sat28/githubcommit
-https://stackoverflow.com/questions/34941546/is-there-a-way-to-integrate-git-with-jupyter-and-have-a-version-control-over-the
